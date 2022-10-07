ea59bea5c4d02b0632f6ce725119daf06e43bcdc
Francisco Jaramillo <pacho@live.ca>
2022-10-07 11:25:56 -0400

    move animations to their own file

---
+++ src/animations.ts
@@ -0,0 +1,20 @@
+import { Clock, Object3D } from 'three'
+
+function rotate(object: Object3D, clock: Clock, radiansPerSecond: number = Math.PI / 2) {
+  const rotationAngle = clock.getDelta() * radiansPerSecond
+  object.rotateY(rotationAngle)
+}
+
+function bounce(
+  object: Object3D,
+  clock: Clock,
+  bounceSpeed: number = 1.5,
+  amplitude: number = 0.4,
+  yLowerBound: number = 0.5
+) {
+  const elapsed = clock.getElapsedTime()
+  const yPos = Math.abs(Math.sin(elapsed * bounceSpeed) * amplitude)
+  object.position.y = yPos + yLowerBound
+}
+
+export { rotate, bounce }
